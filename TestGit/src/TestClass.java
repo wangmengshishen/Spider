0154a7541909b14f3205014a8551c5e7d7edf803
wangmengshishen <wangmengshishen@gmail.com>
2017-04-20 17:50:37 +0800

    s

--- TestGit/src/TestClass.java
+++ TestGit/src/TestClass.java
@@ -3,6 +3,12 @@ public class TestClass {
 	public void testFunction(){
 		System.out.println("hello....");
 	}
+	public void testFunction1(){
+		System.out.println("hello....");
+	}
+	public void testFunction2(){
+		System.out.println("hello....");
+	}
 public static void main(String[] args) {
 	System.out.println("nihao......");
 }
